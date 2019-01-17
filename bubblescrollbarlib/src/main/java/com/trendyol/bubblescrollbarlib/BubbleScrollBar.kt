b3e32eeb4016e80d52e93993c746d5f7a40e7c7e
Nicholas Holley <npholley@gmail.com>
2019-01-17 13:37:50 -0500

    allow ability to continue scrolling off the scrollbar

--- bubblescrollbarlib/src/main/java/com/trendyol/bubblescrollbarlib/BubbleScrollBar.kt
+++ bubblescrollbarlib/src/main/java/com/trendyol/bubblescrollbarlib/BubbleScrollBar.kt
@@ -135,6 +135,8 @@ class BubbleScrollBar : FrameLayout {
 
     private var thumbBackground: Drawable? = null
 
+    private var allowScrollOutside: Boolean = false
+
     private fun obtainStyledAttributes(attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0) {
         with(context.theme.obtainStyledAttributes(attrs, R.styleable.BubbleScrollBar, defStyleAttr, defStyleRes)) {
             scrollBarBackground = getDrawable(R.styleable.BubbleScrollBar_scrollbarBackground)
@@ -172,6 +174,7 @@ class BubbleScrollBar : FrameLayout {
                 R.styleable.BubbleScrollBar_bubbleHeight,
                 R.dimen.default_bubble_height
             )
+            allowScrollOutside = getBoolean(R.styleable.BubbleScrollBar_allowScrollOutside, false)
             return@with
         }
 
@@ -275,7 +278,7 @@ class BubbleScrollBar : FrameLayout {
     }
 
     private fun shouldContinueFastScrolling(event: MotionEvent): Boolean {
-        return event.action == MotionEvent.ACTION_MOVE && isEventInScrollBarPosition(event)
+        return event.action == MotionEvent.ACTION_MOVE && (allowScrollOutside || isEventInScrollBarPosition(event))
     }
 
     private fun shouldStartFastScrolling(event: MotionEvent): Boolean {
